d969f2171b2aabc771dbb71479ef26dde45bcab8
gwak-amole <gwaksophie@gmail.com>
2026-07-14 23:05:18 -0600

    Add conditional collision
    For the player so that when it dodges left or right the collision shape matches

--- scripts/left_obstacle.gd
+++ scripts/left_obstacle.gd
@@ -10,9 +10,9 @@ func _physics_process(delta: float) -> void:
 	velocity.z = direction.z * SPEED
 	
 	
-	
 	move_and_slide()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("OI HIT")
+	if body.name == "player":
+		print("OI HIT")
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,19 +4,44 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export var anim: AnimatedSprite3D
+@export var collider: CollisionShape3D
+@export var left_collider: CollisionShape3D
+@export var right_collider: CollisionShape3D
+@export var limbo_collider: CollisionShape3D
+
 
 func _ready():
 	anim.play("run");
+	left_collider.set_deferred("disabled", true)
+	right_collider.set_deferred("disabled", true)
+	limbo_collider.set_deferred("disabled", true)
+	
 	
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("z"):
 		anim.play("dodge_to_left")
+		collider.set_deferred("disabled", true)
+		left_collider.set_deferred("disabled", false)
+		right_collider.set_deferred("disabled", true)
+		limbo_collider.set_deferred("disabled", true)
 	elif Input.is_action_just_pressed("v"):
 		anim.play("dodge_to_right")
+		collider.set_deferred("disabled", true)
+		left_collider.set_deferred("disabled", true)
+		right_collider.set_deferred("disabled", false)
+		limbo_collider.set_deferred("disabled", true)
 	elif Input.is_action_just_pressed("c"):
 		anim.play("limbo");
+		collider.set_deferred("disabled", true)
+		left_collider.set_deferred("disabled", true)
+		right_collider.set_deferred("disabled", true)
+		limbo_collider.set_deferred("disabled", false)
 	elif Input.is_action_just_pressed("x"):
 		anim.play("run")
+		collider.set_deferred("disabled", false)
+		left_collider.set_deferred("disabled", true)
+		right_collider.set_deferred("disabled", true)
+		limbo_collider.set_deferred("disabled", true)
 
 	move_and_slide()
