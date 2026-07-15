b4c64813638e37c3990d84e9c11697e392637ab1
gwak-amole <gwaksophie@gmail.com>
2026-07-15 15:25:09 -0600

    Fix queue_free obstacle mech

---
+++ scripts/game_controller.gd
@@ -0,0 +1,18 @@
+extends Node
+signal faster
+@export var timer: Timer;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	faster.emit()
+	timer.start()
+	
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,29 +1,30 @@
 extends Node3D
 @export var obstacle = preload("res://scenes/obstacle.tscn")
+@export var gc: Node
+@export var timer: Timer
 var rng = RandomNumberGenerator.new();
 var chance_array = [1, 2, 3];
 var weights = PackedFloat32Array([1, 1, 1.2])
 var chance: int;
+var spawn_cap = 3
+var obs_in_scene = 0;
 
 func _ready() -> void:
 	chance = chance_array[rng.rand_weighted(weights)];
-
+	gc.connect("faster", increase_spawn_freq);
+	obs_in_scene = 0;
 
 func _process(delta: float) -> void:
 	pass
 	
 	
 func _spawn(spawn_pos: Vector3):
-	var obj = obstacle.instantiate();
-	add_child(obj);
-	obj.global_position = spawn_pos
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.name.containsn("obstacle"):
-		print("queue freed")
-		body.queue_free();
-
+	if obs_in_scene < spawn_cap:
+		var obj = obstacle.instantiate();
+		print(obj.name);
+		add_child(obj);
+		obs_in_scene += 1;
+		obj.global_position = spawn_pos
 
 func _on_timer_timeout() -> void:
 	if chance == 1:
@@ -33,4 +34,13 @@ func _on_timer_timeout() -> void:
 	elif chance == 3:
 		_spawn(Vector3(0, 3.9, -10));
 	chance = chance_array[rng.rand_weighted(weights)]; 
-	$Timer.start();
+	timer.start();
+
+func increase_spawn_freq():
+	timer.wait_time *= 0.9
+	print(timer.wait_time)
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.name == "obstacle_area":
+		area.queue_free();
+		obs_in_scene-=1;
