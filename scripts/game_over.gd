a5de7ea492f95da1b7a69271b10913c2a3b7a459
gwak-amole <gwaksophie@gmail.com>
2026-07-16 19:19:36 -0600

    Add start screen

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -31,4 +31,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("x"):
+		pass
