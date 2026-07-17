e5d27dcabb1e9a8b644791d7d7deecbd6bf5305a
gwak-amole <gwaksophie@gmail.com>
2026-07-17 11:57:21 -0600

    Add blinking animation to start

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,9 +1,17 @@
 extends Control
+@export var anim_blink: AnimatedSprite2D
+@export var anim: AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim_blink.show()
+	get_tree().paused = true
+	anim_blink.play("default")
+	await anim_blink.animation_finished
+	anim_blink.hide()
+	get_tree().paused = false
+	anim.play("start_screen_movement")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
