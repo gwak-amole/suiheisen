22f0271c45bcfe2146dcbe439731913f769ba6fc
gwak-amole <gwaksophie@gmail.com>
2026-07-17 16:58:16 -0600

    Add delay to game start

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,13 +1,17 @@
 extends Control
 @export var anim_blink: AnimatedSprite2D
 @export var anim: AnimationPlayer
+@export var black: ColorRect
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim_blink.show()
+	black.show()
+	await get_tree().create_timer(2.0).timeout
 	get_tree().paused = true
 	anim_blink.play("default")
+	black.hide()
 	await anim_blink.animation_finished
 	anim_blink.hide()
 	get_tree().paused = false
@@ -16,4 +20,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("x"):
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
