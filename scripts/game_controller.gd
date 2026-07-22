1bf3b53db046c4595c8ae4c7e289628002e49efe
gwak-amole <gwaksophie@gmail.com>
2026-07-22 15:41:03 -0600

    Add pause menu

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -9,13 +9,17 @@ signal game_over
 @export var anim: AnimationPlayer
 @export var fade_texture: ColorRect
 @export var instructions: Node
+@export var pause_menu: Panel
 var hit = false;
 var points: int = 0;
 var points_inc = 5;
 var instructions_shown = true;
+var pause_shown = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pause_shown = false
+	pause_menu.hide()
 	get_tree().paused = true
 	instructions_shown = true;
 	instructions.show()
@@ -30,6 +34,11 @@ func _process(delta: float) -> void:
 		instructions_shown = false;
 		instructions.hide();
 		get_tree().paused = false;
+	if !instructions_shown and Input.is_action_just_pressed("ui_cancel") and !pause_shown:
+		pause_menu.show();
+		get_tree().paused = true
+		pause_shown = true;
+		
 func _on_timer_timeout() -> void:
 	points_inc *= 1.02;
 	faster.emit()
@@ -50,3 +59,15 @@ func hurt():
 		await anim.animation_finished
 		if is_inside_tree():
 			get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+
+
+func _on_resume_pressed() -> void:
+	pause_shown = false
+	pause_menu.hide();
+	get_tree().paused = false;
+
+
+func _on_quit_pressed() -> void:
+	pause_shown = false
+	get_tree().paused = false;
+	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
