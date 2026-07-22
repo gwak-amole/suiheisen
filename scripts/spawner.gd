c3df2d4deea97d80f5e150a0dcb1868940ed68af
gwak-amole <gwaksophie@gmail.com>
2026-07-22 12:00:05 -0600

    Adjust obstacle spawn z location

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -10,7 +10,7 @@ var rng = RandomNumberGenerator.new();
 var chance_array = [1, 2, 3];
 var weights = PackedFloat32Array([1, 1, 1.2])
 var chance: int;
-var spawn_cap = 3
+var spawn_cap = 6
 var obs_in_scene = 0;
 var cooldown = 1.0
 var just_spawned = false;
@@ -46,17 +46,17 @@ func _spawn(spawn_pos: Vector3, isLimbo: bool):
 
 func _on_timer_timeout() -> void:
 	if chance == 1:
-		_spawn(Vector3(0, 0, -15), false); 
+		_spawn(Vector3(0, 0, -30), false); 
 	elif chance == 2:
-		_spawn(Vector3(0, 0, -15), false);
+		_spawn(Vector3(0, 0, -30), false);
 	elif chance == 3:
-		_spawn(Vector3(0, 0, -15), true);
+		_spawn(Vector3(0, 0, -30), true);
 	chance = chance_array[rng.rand_weighted(weights)]; 
 	timer.start();
 
 func increase_spawn_freq():
-	timer.wait_time *= 0.99
-	cooldown *= 0.9
+	timer.wait_time *= 0.98
+	cooldown *= 0.88
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.name.contains("obstacle_area"):
